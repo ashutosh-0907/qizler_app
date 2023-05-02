e25ad969794ab0849654996e772483001b2a1bd5
ashutosz <ashutoshrai08022003@gmail.com>
2023-05-02 19:23:48 +0530

    adding some function

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<String> scoreKeeper = [
-   'You can lead a cow down stairs but not up stairs.'
-'Approximately one quarter of human bones are in the feet.'
- 'A slug\'s blood is green.'
+    'You can lead a cow down stairs but not up stairs',
+
+    'Approximately one quarter of human bones are in the feet.',
+
+    'A slug\'s blood is green.',
   ];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                scoreKeeper[0],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -57,19 +61,18 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-              ),
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
                 ),
-              ),
-              onPressed: () {                
-                }              
-            ),
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {}),
           ),
         ),
         Expanded(
@@ -91,8 +94,8 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         Row(
-          children: scoreKeeper,
-        ),
+            //children: scoreKeeper(),
+            ),
       ],
     );
   }
